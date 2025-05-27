c8fbe29acf8bde426822f52d9240bedfc3ff2f1a
Vivek Vatsa <72134769+Vivz-cyber@users.noreply.github.com>
2025-05-27 06:18:39 +0530

    fourth commit

--- src/main/java/com/example/Product_Midware/Controller/ProductProxyController.java
+++ src/main/java/com/example/Product_Midware/Controller/ProductProxyController.java
@@ -1,5 +1,6 @@
 package com.example.Product_Midware.Controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/example/Product_Midware/DTO/ProductDto.java
+++ src/main/java/com/example/Product_Midware/DTO/ProductDto.java
@@ -1,8 +1,11 @@
 package com.example.Product_Midware.DTO;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ProductDto {
     private Long id;
     private String name;
