25058f71cc3ef013f778aa74a2f47f95cf85221d
Vivek Vatsa <72134769+Vivz-cyber@users.noreply.github.com>
2025-05-27 06:44:09 +0530

    fifth commit

--- src/main/java/com/example/Product_Midware/ProductMidwareApplication.java
+++ src/main/java/com/example/Product_Midware/ProductMidwareApplication.java
@@ -2,12 +2,19 @@ package com.example.Product_Midware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProductMidwareApplication {
+public class ProductMidwareApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductMidwareApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ProductMidwareApplication.class);
+
+	}
 }
